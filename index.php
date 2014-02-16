dc7fc608609865edb57240c473bdfc2099ce5675
Kelly Tweddell <kelly.tweddell@gmail.com>
2014-02-16 02:40:14 -0700

    added Anagram logo

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php include('header.php'); ?>
-<h1>Sweet Board</h1>
+<img>../img/anagram_logo.png</img>
 <section class="wrapper">
 	<?php include('get/init.php'); ?>
 </section>
 <section class="vintage">
-	<h3><em>Vintage</em> sweetness</h3>
+	<h3>Oldies but goodies</h3>
 	<ul class="slider">
 	</ul>
 </section>
 <footer>
-	<div>To add to the sweetness, text <span>(212) 555-8630</span></div>
+	<div>To add to the sweetness, text <span>(587) 315-3009</span></div>
 </footer>
 <?php include('footer.php'); ?>
